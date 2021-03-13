57e336bdab41fd98c9ab3c96fe087c08597c3020
Randhy <randhypradanairsan@gmail.com>
2021-03-13 22:40:24 +0700

    Initial commit

---
+++ app/src/main/java/com/randhypi/mynavigationdrawer/ui/cart/CartFragment.kt
@@ -0,0 +1,24 @@
+package com.randhypi.mynavigationdrawer.ui.cart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.randhypi.mynavigationdrawer.R
+
+
+class CartFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+
+
+    }
+
+}
